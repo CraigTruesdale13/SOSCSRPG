f73a5e2d92bc028dd4ca41b78503836ad947e42b
Craig Truesdale <catmandew13@gmail.com>
2020-02-15 16:11:08 -0800

    6.2 added ability to use quests in game and view them on the UI

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -24,6 +24,8 @@ namespace Engine.ViewModels
                 OnPropertyChanged(nameof (HasLocationToWest));
                 OnPropertyChanged(nameof (HasLocationToEast));
                 OnPropertyChanged(nameof (HasLocationToSouth));
+
+                GivePlayerQuestsAtLocation();
             }
         }
         public bool HasLocationToNorth
@@ -103,6 +105,16 @@ namespace Engine.ViewModels
             if(HasLocationToSouth)
             {
                 CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1);
+            }
+        }
+        private void GivePlayerQuestsAtLocation()
+        {
+            foreach(Quest quest in CurrentLocation.QuestsAvailableHere)
+            {
+                if(!CurrentPlayer.Quests.Any(q => q.PlayerQuest.ID == quest.ID))
+                {
+                    CurrentPlayer.Quests.Add(new QuestStatus(quest));
+                }
             }
         }
 
