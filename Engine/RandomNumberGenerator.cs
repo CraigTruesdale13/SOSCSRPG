0ae014698a1e8b7a3d1eea3613d38aafb1abbb92
Craig Truesdale <catmandew13@gmail.com>
2020-02-15 17:03:41 -0800

    7.1 Created monsters with loot tables

---
+++ Engine/RandomNumberGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Engine
+{
+    //This is the more complex version
+    public static class RandomNumberGenerator
+    {
+        private static readonly RNGCryptoServiceProvider _generator = new RNGCryptoServiceProvider();
+
+        public static int NumberBetween(int minimumValue, int maximumValue)
+        {
+            byte[] randomNumber = new byte[1];
+            _generator.GetBytes(randomNumber);
+            double asciiValueOfRandomCharacter = Convert.ToDouble(randomNumber[0]);
+            /* We are using Math.Max and subtracting .00000000001,
+             * to ensure "muliplier" will always be between 0.0  and .99999999999
+             * Otherwise, it's possible for it to be "1" which causes problems in our rounding
+             */
+            double multiplier = Math.Max(0, (asciiValueOfRandomCharacter / 255d) - 0.00000000001d);
+            int range = maximumValue - minimumValue + 1;
+            double randomValueInRange = Math.Floor(multiplier * range);
+            return (int)(minimumValue + randomValueInRange);
+        }
+    }
+}
