5b049191463512c0bf88d4d34459c0961a676fab
Craig Truesdale <catmandew13@gmail.com>
2020-02-16 22:54:22 -0800

    10.6  Clean up property setters and property change notifications

--- Engine/Models/World.cs
+++ Engine/Models/World.cs
@@ -5,17 +5,14 @@ namespace Engine.Models
     public class World
     {
         private readonly List<Location> _locations = new List<Location>();
-        internal void AddLocation(int xCoordinate, int yCoordinate, string name, string description, string imageName)
-        {
-            Location loc = new Location();
-            loc.XCoordinate = xCoordinate;
-            loc.YCoordinate = yCoordinate;
-            loc.Name = name;
-            loc.Description = description;
-            loc.ImageName = $"/Engine;component/Images/Locations/{imageName}";
 
-            _locations.Add(loc);
+        internal void AddLocation(int xCoordinate, int yCoordinate,
+                                  string name, string description, string imageName)
+        {
+            _locations.Add(new Location(xCoordinate, yCoordinate, name, description,
+                           $"/Engine;component/Images/Locations/{imageName}"));
         }
+
         public Location LocationAt(int xCoordinate, int yCoordinate)
         {
             foreach (Location loc in _locations)
@@ -25,6 +22,7 @@ namespace Engine.Models
                     return loc;
                 }
             }
+
             return null;
         }
     }
